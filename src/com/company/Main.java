16249885da565678a7442f2dd3cf60eafe012935
admin <mmajchrzak1998@gmail.com>
2020-05-06 19:52:03 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Podstawowa b = new Pochodna1();
+        b.f();
+        b.g();
+        b.h();
+
+        Pochodna d = new Pochodna1();
+        d.f();
+        d.g();
+        d.h();
+
+        Pochodna1 d1 = new Pochodna1();
+        d1.f();
+        d1.g();
+        d1.h();
+    }
+}
+
+
+abstract class Podstawowa {
+    public abstract void f();
+
+    public abstract void g();
+
+    public void h() {
+        System.out.println("h() w Podstawowa");
+    }
+}
+
+abstract class Pochodna extends Podstawowa {
+    public void f() {
+        System.out.println("f() w Pochodna");
+    }
+}
+
+class Pochodna1 extends Pochodna {
+    public void g() {
+        System.out.println("g() w Pochodna1");
+    }
+}
+
